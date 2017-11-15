c0919fae887f3e4efb3958fca15b21141ba0677b
maastahraik <rafik.zebdi@gmail.com>
2017-11-16 00:04:50 +0100

    Added dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { IonicApp, IonicErrorHandler, IonicModule, Toast } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 import { Geolocation } from '@ionic-native/geolocation';
+import { LocationAccuracy} from '@ionic-native/location-accuracy';
 import { Diagnostic } from '@ionic-native/diagnostic';
+import { Storage} from '@ionic/storage';
+import { LocalNotifications } from '@ionic-native/local-notifications';
+import { Globalization } from '@ionic-native/globalization';
+import { AndroidPermissions } from '@ionic-native/android-permissions';
+import { BackgroundGeolocation, BackgroundGeolocationConfig, BackgroundGeolocationResponse } from '@ionic-native/background-geolocation';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -32,7 +38,14 @@ import { HttpModule } from '@angular/http';
     {provide: ErrorHandler, useClass: IonicErrorHandler},
     OwmService,
     Geolocation,
-    Diagnostic
+    Diagnostic,
+    LocationAccuracy,
+    Storage,
+    LocalNotifications,
+    Globalization,
+    Toast,
+    AndroidPermissions,
+    BackgroundGeolocation
   ]
 })
 export class AppModule {}
